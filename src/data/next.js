58ea4fffd9db8c1c095bc6559256f7be1cee33cb
olenadm <olenad@gmail.com>
2024-06-10 16:47:20 +0200

    Ampersands

--- src/data/next.js
+++ src/data/next.js
@@ -7,7 +7,7 @@ export const next = [
       "This webinar series is presented jointly by the FIA, FIA  Foundation, DCU and the UAOA Coalition Members. The aim of this series is to bring together scholars and sport industry stakeholders from across the globe to disseminate research, share experiences and develop collaborative projects on the subject of online abuse. Online abuse is a significant problem in sport, as  signified by its increased media coverage, growing academic support and the development of initiatives such as the UAOA  Coalition. We hope you enjoy this upcoming webinar as we unite to tackle online abuse in sport.",
     next: true,
     biography:
-      "Adrien Bouchet is the Richard &amp; Helen DeVos Foundation Endowed Chair and Eminent Scholar of the DeVos Sport Business Management Programme and Director of The Institute for Diversity and Ethics in Sport (TIDES), which releases the widely publicized Racial and Gender Report Cards for professional, collegiate, and amateur sports each year. Bouchet came to UCF from the University of Tulsa where he spent 12 years as the Warren Clinic Endowed Professor, Sport Business.",
+      "Adrien Bouchet is the Richard & Helen DeVos Foundation Endowed Chair and Eminent Scholar of the DeVos Sport Business Management Programme and Director of The Institute for Diversity and Ethics in Sport (TIDES), which releases the widely publicized Racial and Gender Report Cards for professional, collegiate, and amateur sports each year. Bouchet came to UCF from the University of Tulsa where he spent 12 years as the Warren Clinic Endowed Professor, Sport Business.",
     img: "Adrien_Bouchet.jpg",
   },
 ];
